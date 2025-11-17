d8adf20a03538b3cddc542356e5a1c255d23718f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-11-17 15:33:31 -0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,61 @@
+// swift-tools-version:6.0
+
+//
+// This source file is part of the TemplatePackage open source project
+// 
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+// 
+// SPDX-License-Identifier: MIT
+//
+
+import class Foundation.ProcessInfo
+import PackageDescription
+
+
+let package = Package(
+    name: "TemplatePackage",
+    platforms: [
+        .iOS(.v17),
+        .watchOS(.v10),
+        .visionOS(.v1),
+        .tvOS(.v17),
+        .macOS(.v14),
+        .macCatalyst(.v17)
+    ],
+    products: [
+        .library(name: "TemplatePackage", targets: ["TemplatePackage"])
+    ],
+    dependencies: [
+    ] + swiftLintPackage(),
+    targets: [
+        .target(
+            name: "TemplatePackage",
+            plugins: [] + swiftLintPlugin()
+        ),
+        .testTarget(
+            name: "TemplatePackageTests",
+            dependencies: [
+                .target(name: "TemplatePackage")
+            ],
+            plugins: [] + swiftLintPlugin()
+        )
+    ]
+)
+
+
+func swiftLintPlugin() -> [Target.PluginUsage] {
+    // Fully quit Xcode and open again with `open --env SPEZI_DEVELOPMENT_SWIFTLINT /Applications/Xcode.app`
+    if ProcessInfo.processInfo.environment["SPEZI_DEVELOPMENT_SWIFTLINT"] != nil {
+        [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+    } else {
+        []
+    }
+}
+
+func swiftLintPackage() -> [PackageDescription.Package.Dependency] {
+    if ProcessInfo.processInfo.environment["SPEZI_DEVELOPMENT_SWIFTLINT"] != nil {
+        [.package(url: "https://github.com/realm/SwiftLint.git", from: "0.55.1")]
+    } else {
+        []
+    }
+}
